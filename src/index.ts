d047bab4025526d50c0a80e9fb49e349093cded5
Prashant-00-00 <sharma.prashant.8375@gmail.com>
2024-10-10 04:17:06 +0530

    Added routing

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,20 @@ import * as cron from 'node-cron';
 import fetchCryptoPrices from './fetchPrice';
 import connectDB from './database/db';
 import { router } from './api/stats';
+import { deviationrouter } from './api/deviation';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/stats', router);
+app.use('/deviation', deviationrouter);
 
 connectDB();
 
+fetchCryptoPrices();
 cron.schedule('0 */2 * * *', () => {
   console.log('Fetching cryptocurrency prices...');
-  fetchCryptoPrices();
 });
 
 app.listen(PORT, () => {
